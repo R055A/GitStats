b9d7eb025359f35b3d10ae23f1af3457adc22191
Adam Ross <14985050+R055A@users.noreply.github.com>
2023-10-18 20:54:27 +0200

    Add owned repo percentage stat

--- src/github_repo_stats.py
+++ src/github_repo_stats.py
@@ -36,6 +36,7 @@ class GitHubRepoStats(object):
         self._languages: Optional[Dict[str, Any]] = None
         self._excluded_languages: Optional[Set[str]] = None
         self._repos: Optional[Set[str]] = None
+        self._owned_repos: Optional[Set[str]] = None
         self._users_lines_changed: Optional[Tuple[int, int]] = None
         self._avg_percent: Optional[str] = None
         self._views: Optional[int] = None
@@ -326,13 +327,25 @@ class GitHubRepoStats(object):
     @property
     async def repos(self) -> Set[str]:
         """
-        :return: list of names of user's repos
+        :return: list of names of repos user is involved with
         """
         if self._repos is not None:
             return self._repos
         await self.get_stats()
         assert self._repos is not None
         return self._repos
+
+    @property
+    async def owned_repos(self) -> Set[str]:
+        """
+        :return: list of names of repos owned by user
+        """
+        if self._owned_repos is not None:
+            return self._owned_repos
+        await self.get_stats()
+        assert self._repos is not None
+        self._owned_repos = set([i for i in self._repos if self.environment_vars.username == i.split('/')[0]])
+        return self._owned_repos
 
     @property
     async def total_contributions(self) -> int:
